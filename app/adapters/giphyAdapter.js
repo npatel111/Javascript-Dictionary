c763a76b4e9f219ee327f3c921896799d89eaab5
drzackyll <adams.z.d@gmail.com>
2016-11-14 17:37:57 -0500

    Fixes random gif

--- app/adapters/giphyAdapter.js
+++ app/adapters/giphyAdapter.js
@@ -5,16 +5,16 @@ function getGif(word) {
 }).done(function (response) {
     displayGif(response)
     // //debugger
-    $('#gif').append(`<button id="anotherGif" onclick="nextGif('${word}')">and another!</button>`)
+    // $('#gif').append(`<button id="anotherGif" onclick="displayGif('${response}')">and another!</button>`)
   })
 }
-
-function nextGif(word) {
-  // //debugger
-  $.ajax({
-  method: "GET",
-  url: `http://api.giphy.com/v1/gifs/search?q=${word}&api_key=dc6zaTOxFJmzC`,
-  }).done(function (response) {
-    displayRandomGif(response)
-  })
-}
+//
+// function nextGif(word) {
+//   // //debugger
+//   $.ajax({
+//   method: "GET",
+//   url: `http://api.giphy.com/v1/gifs/search?q=${word}&api_key=dc6zaTOxFJmzC`,
+//   }).done(function (response) {
+//     displayRandomGif(response)
+//   })
+// }
--- app/index.js
+++ app/index.js
@@ -1,52 +1,19 @@
-// (function () {
-//   $('#clickme').hide()
-// }())
 
 $(document).ready(function() { // on document ready
   newForm()
   $('#clickme').hide()
   $('#anotherGif').hide()
-  // tasksController = new tasksController();
-  // tasksController.init();
 })
-
-// // listsController.prototype.init = function () {
-// //   this.$addTaskForm.hide()
-// // }
 
 function newForm() {
   $('form').on('submit', function(){
     event.preventDefault()
     $('form').hide()
     $('#clickme').show()
-    // $('#anotherGif').show()
-    //debugger
     createWord($('#word').val())
     createGif($('#word').val())
   })
 }
-
-// function anotherForm() {
-//   // //debugger
-//   $('form #word').empty()
-//   // //debugger
-//   $('#search').empty()
-//   $('#definition').empty()
-//   $('#gif').empty()
-//   $('form').show()
-//   $('#clickme').hide()
-//   // newForm()
-//   $('form').on('submit', function(){
-//     event.preventDefault()
-//     $('form').hide()
-//     $('#clickme').show()
-//     $('#anotherGif').show()
-//     //debugger
-//     createWord($('#word').val())
-//     createGif($('#word').val())
-//     // createDefinition($('#word').val())
-//   })
-// }
 
 $(function (){
   $('#random').on('click', function(){
@@ -54,57 +21,12 @@ $(function (){
     $('form').hide()
     $('#clickme').show()
     randomWord()
-    // createDefinition($('#word').val())
   })
 })
-
-// $(function (){
-//   $('form').on('submit', function(){
-//     event.preventDefault()
-//     $('form').hide()
-//     $('#clickme').show()
-//     createWord($('#word').val())
-//     createGif($('#word').val())
-//     // createDefinition($('#word').val())
-//   })
-// })
-
-// function randomWord() {
-//   let requestStr = "http://randomword.setgetgo.com/get.php";
-//   $.ajax({
-//     type: "GET",
-//     url: requestStr,
-//     dataType: "jsonp",
-//     jsonpCallback: 'RandomWordComplete'
-//   }).done(function (response) {
-//     createWord(response.Word)
-//     createGif(response.Word)
-//     // //debugger
-//   })
-// }
 
 function displayWord(word) {
   $('#search').append(word)
 }
-
-// function getDefinition(word) {
-//   // //debugger
-//   $.ajax({
-//   method: "GET",
-//   url: `https://mashape-community-urban-dictionary.p.mashape.com/define?term=${word.word}`,
-//   beforeSend: function(xhr) {
-//   xhr.setRequestHeader("X-Mashape-Authorization", "0Pj9sD3WmEmshc4ksOMtS4dyEOGIp1aNbr3jsnrxphIFkVMYVh");
-//   }
-//   }).done(function (response) {
-//     //debugger
-//     if (response.result_type === "no_results") { displayDefinition(response)} else {
-//       let definition = new Definition(response.list[0].definition)
-//       word.definition = definition
-//       word.example = response.list[0].example
-//       displayDefinition(response)
-//     }
-//   })
-// }
 
 function displayDefinition(response) {
   let firstdef = response.list[0]
@@ -116,23 +38,28 @@ function displayDefinition(response) {
 }
 
 function displayGif(response) {
+  debugger
   let firstgif = response.data[0]
   if (!firstgif) {
     $('#gif').append(`<img src="images/giphy.gif" />`)
   }else{
-    store.gifs[store.gifs.length-1].url = firstgif.images.original.url //stores gif url
-    $('#gif').append(`<img src="${firstgif.images.original.url}" />`)//shows gif
+    let responseLength = response.data.length
+    let randgif = response.data[Math.floor(Math.random()*responseLength)]
+      store.gifs[store.gifs.length-1].url = randgif.images.original.url //stores gif url
+      $('#gif').empty().append(`<img src="${randgif.images.original.url}" />`)//shows gif
+      $('#gifbutton').empty().append(`<button id="anotherGif" onclick="getGif('${store.gifs[store.gifs.length-1].word}')">and another!</button>`)
+    // store.gifs[store.gifs.length-1].url = firstgif.images.original.url //stores gif url
+    // $('#gif').append(`<img src="${firstgif.images.original.url}" />`)//shows gif
   }
 }
 
-function displayRandomGif(response) {
-  let responseLength = response.data.length
-  let randgif = response.data[Math.floor(Math.random()*responseLength)]
-    store.gifs[store.gifs.length-1].url = randgif.images.original.url //stores gif url
-    $('#gif').empty().append(`<img src="${randgif.images.original.url}" />`)//shows gif
-    $('#gif').append(`<button id="anotherGif" onclick="nextGif('${word}')">and another!</button>`)
-
-}
+// function displayRandomGif(response) {
+//   let responseLength = response.data.length
+//   let randgif = response.data[Math.floor(Math.random()*responseLength)]
+//     store.gifs[store.gifs.length-1].url = randgif.images.original.url //stores gif url
+//     $('#gif').empty().append(`<img src="${randgif.images.original.url}" />`)//shows gif
+//     $('#gif').append(`<button id="anotherGif" onclick="nextGif('${word}')">and another!</button>`)
+// }
 
 $('.raptor').raptorize();
 
@@ -145,16 +72,5 @@ $( "#clickme" ).click(function() {
     setTimeout(function () {
       document.location.reload(false)
     }, 1500);
-    // anotherForm()
   });
 });
-
-// function nextGif(word) {
-//   $.ajax({
-//   method: "GET",
-//   url: `http://api.giphy.com/v1/gifs/search?q=${word}&api_key=dc6zaTOxFJmzC`,
-//   }).done(function (response) {
-//     displayGif(response)
-//     $('#gif').append(`<button id="anotherGif" onclick="nextGif('${word}')">and another!</button>`)
-//   })
-// }
