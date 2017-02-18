63e10ac4edbb339533a22ef487e7c1a944661ba3
Rebecca Yi <rebeccayeyi@gmail.com>
2017-02-17 19:26:06 -0500

    fully responsive

--- app/index.js
+++ app/index.js
@@ -1,50 +1,47 @@
 $(document).ready(function() {
+  $('.raptor-nav').hide()
   newForm()
-  $('#clickme').hide()
-  $('#anotherGif').hide()
 })
 
 function newForm() {
-  $('form').on('submit', function(){
+  $('form').on('submit', function(e){
     event.preventDefault()
-    $('form').hide()
-    $('#clickme').show()
-    createWord($('#word').val())
-    createGif($('#word').val())
+    let word = e.target.children[0].value
+    $('.first-page').hide()
+    $('.raptor-nav').show()
+    getDefinition(word)
+    getGif(word)
+    $('.definition').append(`<p>${word}</p>`)
   })
 }
 
 $(function (){
-  $('#random').on('click', function(){
+  $('.random').on('click', function(){
     event.preventDefault()
-    $('form').hide()
-    $('#clickme').show()
+    $('.first-page').hide()
+    $('.raptor-nav').show()
     randomWord()
   })
 })
-
-function displayWord(word) {
-  $('#search').append(`${word}<br>`)
-}
-
-function displayDefinition(response) {
-  let firstdef = response.list[0]
-  if (!firstdef){
-    $('#definition').append("No definition, but here's a gif to help cope: ")
-  } else {
-    $('#definition').append(`${firstdef.definition}<br><br>${firstdef.example}<br><br>`)
-  }
-}
 
 function displayGif(response) {
   let firstgif = response.data[0]
   if (!firstgif) {
     getGif("puppies")
-  } else {
+  }
+  else {
     let responseLength = response.data.length
     let randgif = response.data[Math.floor(Math.random()*responseLength)]
-    store.gifs[store.gifs.length-1].url = randgif.images.original.url
-    $('#gif').empty().append(`<img src="${randgif.images.original.url}" /><br><span id="gifbutton"><button id="anotherGif" onclick="getGif('${store.gifs[store.gifs.length-1].word}')">and another!</button></span>`)
+    $('.gif').empty().append(`<button id="anotherGif" onclick="getGif('${$gif}')">and another!</button><img src="${randgif.images.original.url}"/>`)
+  }
+}
+
+function displayDefinition(response) {
+  let def = response.list[0]
+  if (!def){
+    $('.definition').append("No definition, but here's a gif to help cope: ")
+  } else {
+    $('.definition').append(`<p>${def.definition}</p><br><p>${def.example}</p>`)
   }
 }
 
@@ -55,9 +52,9 @@ $( "#clickme" ).click(function() {
     opacity: 0.25,
     left: "+=50",
     width: "toggle"
-  }, 1000, function() {
+  }, 2000, function() {
     setTimeout(function () {
       document.location.reload(false)
-    }, 1500);
+    }, 1000);
   });
 });
